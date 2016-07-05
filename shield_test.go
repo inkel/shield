e3e73f45c5a73f589dadd42e111e411545c30e51
Leandro López <inkel.ar@gmail.com>
2016-07-05 15:39:13 -0300

    Add benchmarks for Check and Encrypt

--- shield_test.go
+++ shield_test.go
@@ -62,3 +62,21 @@ func TestSanity(t *testing.T) {
 		t.Error("password should have checked against encrypted version")
 	}
 }
+
+func BenchmarkCheck(b *testing.B) {
+	crypted := []byte("8cc55858f341586bde60d595d376fdafc4535d94a7383231f2adf323b5c508d2bdddd75b783b2c3acb196334288402406041cb1114ed13e6b96443b0aafccd5esalt")
+	password := []byte("password")
+
+	for i := 0; i < b.N; i++ {
+		Check(password, crypted)
+	}
+}
+
+func BenchmarkEncrypt(b *testing.B) {
+	password := []byte("password")
+	salt := []byte("salt")
+
+	for i := 0; i < b.N; i++ {
+		Encrypt(password, salt)
+	}
+}
